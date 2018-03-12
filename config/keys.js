26df04a91a65436834dd24b498995d48b7c4ea43
admin <Mistermo716@gmail.com>
2018-03-12 16:49:40 -0700

    Google Auth Setup

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+'use strict';
+module.exports = {
+  googleClientID:
+    '181183699038-bphn1n47m0kvhgc9ff8q75qohd4qpqbl.apps.googleusercontent.com',
+  googleClientSecret: 'yBUe9FTt6KiBCx1R-2YANwqO',
+};
