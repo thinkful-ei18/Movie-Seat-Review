84ceebd9f40c346d6047d4155694403c0348d03e
admin <Muaath.Alaraj@gmail.com>
2018-04-11 20:21:24 -0700

    className issue fix

--- client/src/components/Reviews.js
+++ client/src/components/Reviews.js
@@ -15,11 +15,11 @@ class Reviews extends Component {
     const reviews = this.props.reviews.map(review => {
       return (
         <li key={review.id}>
-          <div class="row">
-            <div class="col s12 m6">
-              <div class="card blue-grey darken-1">
-                <div class="card-content white-text">
-                  <span class="card-title">{review.productName}</span>
+          <div className="row">
+            <div className="col s12 m6">
+              <div className="card blue-grey darken-1">
+                <div className="card-content white-text">
+                  <span className="card-title">{review.productName}</span>
                   <img style={imgStyle} alt="product" src={review.image} />
                   <p>Rating: {review.rating} out of 5</p>
                   <p>{review.summary}</p>
